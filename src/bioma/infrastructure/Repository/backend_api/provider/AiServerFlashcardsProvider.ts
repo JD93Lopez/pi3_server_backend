2245030db1bc932698f381b93c094230db7b7ea9
JD93Lopez <juandavid.jdlea@gmail.com>
2025-04-25 23:05:57 -0500

    Port change

--- src/bioma/infrastructure/Repository/backend_api/provider/AiServerFlashcardsProvider.ts
+++ src/bioma/infrastructure/Repository/backend_api/provider/AiServerFlashcardsProvider.ts
@@ -1,8 +1,11 @@
 import AiFlashcards from "../../../../domain/docs/AiFlashcards";
 
 export default class AiServerFlashcardsProvider {
+
+    private readonly baseUrl = 'https://wh37l4d7-5001.use2.devtunnels.ms';
+
     public async getFlashcardsAiTopicId(TopicId: number): Promise<AiFlashcards> {
-        const response = await fetch(`https://wh37l4d7-5000.use2.devtunnels.ms/api/cards/get?cards_id=${TopicId}`);
+        const response = await fetch(`${this.baseUrl}/api/cards/get?cards_id=${TopicId}`);
         if (!response.ok) {
             throw new Error('Network response was not ok ai flashcards');
         }
@@ -11,7 +14,7 @@ export default class AiServerFlashcardsProvider {
     }
 
     public async createFlashcardsFromAi(topicId: number, nombreTema: string, descripcion: string, wishedNumberOfCards: number): Promise<any> {
-        const response = await fetch(`https://wh37l4d7-5000.use2.devtunnels.ms/api/cards?topicId=${topicId}&topicInfo=Información%20del%20tema:Título%20del%20tema:${nombreTema} Explicación:${descripcion} Número%20de%20tarjetas:${wishedNumberOfCards}`);
+        const response = await fetch(`${this.baseUrl}/api/cards?topicId=${topicId}&topicInfo=Información%20del%20tema:Título%20del%20tema:${nombreTema} Explicación:${descripcion} Número%20de%20tarjetas:${wishedNumberOfCards}`);
 
         if (!response.ok) {
             throw new Error('Network response was not ok ai flashcards');
