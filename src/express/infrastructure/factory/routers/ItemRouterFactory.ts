ffc09e57aa278f7c7f80851f484386624eb41cb1
Paola <yenifer.hernandez.2022@upb.edu.co>
2025-04-26 23:58:58 -0500

    Correcciones

--- src/express/infrastructure/factory/routers/ItemRouterFactory.ts
+++ src/express/infrastructure/factory/routers/ItemRouterFactory.ts
@@ -3,6 +3,8 @@ import ItemControllerExpress from "../../../../bioma/infrastructure/express/cont
 import ItemRouterExpress from "../../../../bioma/infrastructure/express/router/ItemRouterExpress"
 import BuyItemServiceFactory from "../../../../bioma/infrastructure/factory/service/Items/BuyItemServiceFactory"
 import RouterExpress from "../../../domain/RouterExpress"
+import GetStoreItemsUseCase from "../../../../bioma/application/usecase/Items/GetStoreItemsUseCase"
+import GetStoreItemsServiceFactory from "../../../../bioma/infrastructure/factory/service/Items/GetStoreItemsServiceFactory"
 
 export default class ItemRouterFactory{
     public static readonly create = (): RouterExpress => {
@@ -12,9 +14,11 @@ export default class ItemRouterFactory{
             const buyItemService = BuyItemServiceFactory.create()
             const buyItemUseCase = new BuyItemUseCase(buyItemService)
     
+            const GetStoreItemsService= GetStoreItemsServiceFactory.create()
+            const GetStoreItems = new GetStoreItemsUseCase(GetStoreItemsService)
     
             
-            const itemController = new ItemControllerExpress(buyItemUseCase)
+            const itemController = new ItemControllerExpress(buyItemUseCase, GetStoreItems)
     
             const userRouter = new ItemRouterExpress(itemController)
             
